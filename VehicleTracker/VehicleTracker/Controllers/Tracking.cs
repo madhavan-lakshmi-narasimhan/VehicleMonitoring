7effb4bf22468b635351bd726fe94144c2be893f
madhavan-lakshmi-narasimhan <madhavan.lakshminarasimhan@gmail.com>
2021-11-01 00:36:13 +0530

    Base Commit

---
+++ VehicleTracker/VehicleTracker/Controllers/Tracking.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VehicleTracker.Controllers
+{
+    public class Tracking : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
